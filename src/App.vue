1d26d25f86076849844ec300c5bdc68f377a3fa8
JOmar210 <ocervantes@quickapps.mx>
2022-02-11 16:14:33 -0600

    wenas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app" >
     <router-view/>
   </div>
 </template>
@@ -15,6 +11,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #683ab7cb;
 }
 
 #nav {
---
+++ src/components/Index.vue
@@ -0,0 +1,19 @@
+<template >
+        <v-card class="main px-2" >
+            <Header />
+            <p>WENAS</p>
+        </v-card>
+</template>
+<script>
+import Header from './Header'
+export default {
+    components:{Header},
+
+}
+</script>
+<style scoped>
+.main{
+    height:100vh;
+    background-color: rgba(0, 0, 0, 0);
+}
+</style>
